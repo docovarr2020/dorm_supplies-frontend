daf3cd3f72fe8d26ffe174dde10173bb94c8bae2
docovarr2020 <diego_covarrubias@college.harvard.edu>
2018-01-10 16:51:27 -0500

    got front end connected to backend

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
 const form = document.forms[0]
+
+function register_user() {
+	const data = {}
+	if (form.name.value) data.name = form.name.value
+	if (form.email.value) data.email = form.email.value
+	if (form.classYear.value) data.classYear = form.classYear.value
+	if (form.address.value) data.address = form.address.value
+	if (form.password.value) data.password = form.password.value
+	
+	fetch('/register_users', {
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		method: 'POST',
+		body: JSON.stringify(data)
+	}).then(function (res) {
+		res.json()
+		.then(function(user){
+			alert(JSON.stringify(user))
+		})
+	}).catch(function (err) {
+		console.log(err)
+	})
+}
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,17 @@ router.get('/', (req, res, next) => {
     return res.render('index');
 });
 
+router.post('/register_users', (req,res, next) => {
+
+	// send the post request to back end
+	request.post({
+
+		// backend url
+		url: config.apiUrl + '/users',
+
+		// make the old req.body the new one
+		form: req.body
+	}).pipe(res)
+})
+
 module.exports = router;
