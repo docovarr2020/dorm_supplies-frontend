a6f508792beb4d19bcee63c7a804917e3e77f2a6
docovarr2020 <diego_covarrubias@college.harvard.edu>
2018-01-12 13:46:21 -0500

    Push to help Bliss

--- app/js/script.js
+++ app/js/script.js
@@ -1,25 +1,100 @@
-const form = document.forms[0]
+const register_form = document.forms[0]
+const login_form = document.forms[1]
 
 function register_user() {
+	console.log(document.forms[0].name.value)
 	const data = {}
-	if (form.name.value) data.name = form.name.value
-	if (form.email.value) data.email = form.email.value
-	if (form.classYear.value) data.classYear = form.classYear.value
-	if (form.address.value) data.address = form.address.value
-	if (form.password.value) data.password = form.password.value
+	if (register_form.name.value) data.name = register_form.name.value
+	if (register_form.email.value) data.email = register_form.email.value
+	if (register_form.classYear.value) data.classYear = register_form.classYear.value
+	if (register_form.address.value) data.address = register_form.address.value
+	if (register_form.password.value) data.password = register_form.password.value
+	if (register_form.confirm.value) data.confirm = register_form.confirm.value
 	
-	fetch('/register_users', {
+	console.log(data)
+	fetch('/register', {
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		method: 'POST',
 		body: JSON.stringify(data)
 	}).then(function (res) {
+		if(!res.ok) {
+			res.text()
+			.then(function(message) {
+				alert(message)
+			})
+		}
 		res.json()
 		.then(function(user){
+			window.location = '/items'
 			alert(JSON.stringify(user))
 		})
 	}).catch(function (err) {
 		console.log(err)
 	})
 }
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function login_user() {
+	const data = {}
+	if (login_form.email.value) data.email = login_form.email.value
+	if (login_form.password.value) data.password = login_form.password.value
+
+	fetch('/login', {
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		method: 'POST',
+		body: JSON.stringify(data)
+	}).then(function (res) {
+		if(!res.ok) {
+			res.text()
+			.then(function(message) {
+				alert(message)
+			})
+		}
+		res.json()
+		.then(function(data){
+			alert(JSON.stringify(data))
+			localStorage.token = data.token
+			window.location = '/items'
+		})
+	}).catch(function (err) {
+		console.log(err)
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,35 @@ router.get('/', (req, res, next) => {
     return res.render('index');
 });
 
-router.post('/register_users', (req,res, next) => {
+router.get('/items', (req, res, next) => {
+	request.get({
+		url: config.apiUrl + '/items',
+		form: req.body
+	}, (err, response, body) => {
+		if (err) return next(err)
+		if (!body) return next(new Error('Missing Body ' + body))
+		return res.render('items', { items: body })
+	})
+})
+
+router.post('/register', (req,res, next) => {
 
 	// send the post request to back end
 	request.post({
 
 		// backend url
-		url: config.apiUrl + '/users',
+		url: config.apiUrl + '/users/student',
 
 		// make the old req.body the new one
 		form: req.body
 	}).pipe(res)
+})
+
+router.post('/login', (req, res, next) => {
+  request.post({
+    url: config.apiUrl + '/auth/login',
+    form: req.body
+  }).pipe(res)
 })
 
 module.exports = router;
